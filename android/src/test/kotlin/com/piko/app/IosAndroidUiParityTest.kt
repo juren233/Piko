85d494f367ccf121461ae288df97be2400e85dc8
juren233 <juren233@gmail.com>
2026-05-15 13:31:45 +0800

    修复动作面板显示时机和边距问题

--- android/src/test/kotlin/com/piko/app/IosAndroidUiParityTest.kt
+++ android/src/test/kotlin/com/piko/app/IosAndroidUiParityTest.kt
@@ -556,7 +556,7 @@ class IosAndroidUiParityTest {
             "https://github.com/alibaba/xquic.git",
             "PIKO_XQUIC_GIT_TAG \"v1.9.2\"",
             "https://github.com/google/boringssl.git",
-            "CMAKE_SKIP_INSTALL_RULES TRUE",
+            "CMAKE_MACOSX_BUNDLE FALSE",
             "add_library(piko_xquic_ios STATIC",
             "PikoXQuicBridge.cpp",
             "PikoXQuicShim.c",
